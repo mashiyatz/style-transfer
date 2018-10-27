ac60ae1defc4fcdb6fbdeaf850581fff821498aa
Ubuntu <ubuntu@ip-172-31-91-192.ec2.internal>
2018-10-27 23:40:39 +0000

    update

--- evaluate.sh
+++ evaluate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python linear-color-transfer.py --target_image styles/mattcosby.jpg --source_image input/content.png --output_image output/style_colored.png
+python linear-color-transfer.py --target_image styles/style.jpg --source_image input/content.png --output_image output/style_colored.png
 
 python lum-transfer.py --content_image input/content.png --style_image output/style_colored.png --cp_mode lum --output_style_image output/style_lum.png --output_content_image output/content_lum.png --org_content input/content.png
 
-python Network.py "output/content_lum.png" "output/style_lum.png" "output/content" --image_size 1000 --num_iter 20
+python Network.py "output/content_lum.png" "output/style_lum.png" "output/content" --image_size 1000 --num_iter 20 --style_weight 5.0
 
 python lum-transfer.py --output_lum2 output/content_at_iteration_20.png --cp_mode lum2 --output_image output/out_combined.png --org_content input/content.png
 
